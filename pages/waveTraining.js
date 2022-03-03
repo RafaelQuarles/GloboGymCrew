a1289cf5ec8bcfe5d6347c6224a49770cda69edd
R370 <63675087+RafaelQuarles@users.noreply.github.com>
2022-03-03 16:49:31 -0500

    small tweeks

--- pages/waveTraining.js
+++ pages/waveTraining.js
@@ -3,19 +3,19 @@ function toggleSelectLift() {
     let squat = document.getElementById("squat");
     let deadlift = document.getElementById("deadlift");
     
-    document.getElementsByClassName('toggle')[0]
+    document.getElementsByClassName('togglelift')[0]
         .addEventListener('click', function () {
             this.classList.toggle('active');
             squat.classList.remove("active");
             deadlift.classList.remove("active");
         });
-    document.getElementsByClassName('toggle')[1]
+    document.getElementsByClassName('togglelift')[1]
         .addEventListener('click', function () {
             bench.classList.remove("active");
             deadlift.classList.remove("active");
             this.classList.toggle('active');
         });
-    document.getElementsByClassName('toggle')[2]
+    document.getElementsByClassName('togglelift')[2]
         .addEventListener('click', function () {
             bench.classList.remove("active");
             squat.classList.remove("active");
@@ -35,13 +35,13 @@ function waveTracker() {
     const deadliftClass = document.getElementById("deadlift");
 
     if ( benchClass.classList.contains("active") ) {
-        calcWave("bench", parseInt(startnum));
+        calcWave(parseInt(startnum), 10);
     } 
     else if ( squatClass.classList.contains("active") ) {
-        calcWave("squat", parseInt(startnum));
+        calcWave(parseInt(startnum), 20);
     } 
     else if ( deadliftClass.classList.contains("active") ) {
-        calcWave("deadlift", parseInt(startnum));
+        calcWave(parseInt(startnum), 20);
     };
 
     for (let i = 0; i < 12; i++) {
@@ -58,24 +58,9 @@ function waveTracker() {
         toggleCheckX();
     }
 
-    function calcWave(lift, num) {
-        if (lift == "bench") {
-            let wave = [num, num+10, num+20, num+30, num+40, num+50];
-
-            for (let i = 0; i < 4; i++) {
-                document.getElementById(`wave${i+1}`).innerHTML = 
-                `
-                <div> 
-                    WAVE ${i+1}
-                    <div class="set">${wave[0+i]} lbs x 3 reps <div class="xcheck_container"><div class="check">✓</div><div class="xmark" id="xmark">X</div></div></div>
-                    <div class="set">${wave[1+i]} lbs x 2 reps <div class="xcheck_container"><div class="check">✓</div><div class="xmark" id="xmark">X</div></div></div>
-                    <div class="set">${wave[2+i]} lbs x 1 reps <div class="xcheck_container"><div class="check">✓</div><div class="xmark" id="xmark">X</div></div></div>
-                </div>
-                `;  
-            }
-        }
-        else if (lift == "squat" || lift == "deadlift") {
-            let wave = [num, num+20, num+40, num+60, num+80, num+100];
+    function calcWave( num, inc) {
+        if (num != 0 && inc != 0) {
+            let wave = [num, num+inc, num+(inc*2), num+(inc*3), num+(inc*4), num+(inc*5)];
 
             for (let i = 0; i < 4; i++) {
                 document.getElementById(`wave${i+1}`).innerHTML = 
@@ -90,7 +75,7 @@ function waveTracker() {
             }
         }
         else {
-            console.log("Not a valid lift");
+            console.log("Not a valid input");
         }
     }
 }
