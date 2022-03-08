1e2ca410682495b70bd25ded358a01b2bc684743
R370 <63675087+RafaelQuarles@users.noreply.github.com>
2022-03-07 23:41:57 -0500

    753 addition to waves
    Added 753  functionality

--- darkmode.js
+++ darkmode.js
@@ -1,5 +1,3 @@
-// Add this to your javascript file
-
 /* Body */
 const body = document.querySelector('body');
 
--- pages/waveTraining.js
+++ pages/waveTraining.js
@@ -24,11 +24,30 @@ function toggleSelectLift() {
 }
 toggleSelectLift();
 
+function toggleSelectIncrement() {
+    let three21 = document.getElementById("three21");
+    let seven53 = document.getElementById("seven53");
+    
+    document.getElementsByClassName('toggleinc')[0]
+        .addEventListener('click', function () {
+            seven53.classList.remove("active");
+            this.classList.toggle('active');
+        });
+    document.getElementsByClassName('toggleinc')[1]
+        .addEventListener('click', function () {
+            three21.classList.remove("active");
+            this.classList.toggle('active');
+        });
+}
+toggleSelectIncrement();
 
 
 
 function waveTracker() {
     const startnum = document.getElementById("startnum").value;
+
+    const three21Class = document.getElementById("three21");
+    const seven53Class = document.getElementById("seven53");
 
     const benchClass = document.getElementById("bench");
     const squatClass = document.getElementById("squat");
@@ -59,9 +78,8 @@ function waveTracker() {
     }
 
     function calcWave( num, inc) {
-        if (num != 0 && inc != 0) {
+        if (num != 0 && inc != 0 && three21Class.classList.contains("active")) {
             let wave = [num, num+inc, num+(inc*2), num+(inc*3), num+(inc*4), num+(inc*5)];
-
             for (let i = 0; i < 4; i++) {
                 document.getElementById(`wave${i+1}`).innerHTML = 
                 `
@@ -70,6 +88,19 @@ function waveTracker() {
                     <div class="set">${wave[0+i]} lbs x 3 reps <div class="xcheck_container"><div class="check">✓</div><div class="xmark" id="xmark">X</div></div></div>
                     <div class="set">${wave[1+i]} lbs x 2 reps <div class="xcheck_container"><div class="check">✓</div><div class="xmark" id="xmark">X</div></div></div>
                     <div class="set">${wave[2+i]} lbs x 1 reps <div class="xcheck_container"><div class="check">✓</div><div class="xmark" id="xmark">X</div></div></div>
+                </div>
+                `;  
+            }
+        } else if (num != 0 && inc != 0 && seven53Class.classList.contains("active")) {
+            let wave = [num, num+inc, num+(inc*2), num+(inc*3), num+(inc*4), num+(inc*5)];
+            for (let i = 0; i < 4; i++) {
+                document.getElementById(`wave${i+1}`).innerHTML = 
+                `
+                <div> 
+                    WAVE ${i+1}
+                    <div class="set">${wave[0+i]} lbs x 7 reps <div class="xcheck_container"><div class="check">✓</div><div class="xmark" id="xmark">X</div></div></div>
+                    <div class="set">${wave[1+i]} lbs x 5 reps <div class="xcheck_container"><div class="check">✓</div><div class="xmark" id="xmark">X</div></div></div>
+                    <div class="set">${wave[2+i]} lbs x 3 reps <div class="xcheck_container"><div class="check">✓</div><div class="xmark" id="xmark">X</div></div></div>
                 </div>
                 `;  
             }
