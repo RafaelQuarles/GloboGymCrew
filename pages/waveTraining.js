4efc0b70fad12910953668cce57813d30141d7ae
R370 <63675087+RafaelQuarles@users.noreply.github.com>
2022-03-15 16:24:26 -0400

    Weight Math
    Added weight math for a visual display to calculate plates for each set.

--- pages/waveTraining.js
+++ pages/waveTraining.js
@@ -85,9 +85,9 @@ function waveTracker() {
                 `
                 <div> 
                     WAVE ${i+1}
-                    <div class="set">${wave[0+i]} lbs x 3 reps <div class="xcheck_container"><div class="check">✓</div><div class="xmark" id="xmark">X</div></div></div>
-                    <div class="set">${wave[1+i]} lbs x 2 reps <div class="xcheck_container"><div class="check">✓</div><div class="xmark" id="xmark">X</div></div></div>
-                    <div class="set">${wave[2+i]} lbs x 1 reps <div class="xcheck_container"><div class="check">✓</div><div class="xmark" id="xmark">X</div></div></div>
+                    <div class="set"><i class="fas fa-question-circle" onclick="openModal(${wave[0+i]})"></i>${wave[0+i]} lbs x 3 reps <div class="xcheck_container"><div class="check">✓</div><div class="xmark" id="xmark">X</div></div></div>
+                    <div class="set"><i class="fas fa-question-circle" onclick="openModal(${wave[1+i]})"></i>${wave[1+i]} lbs x 2 reps <div class="xcheck_container"><div class="check">✓</div><div class="xmark" id="xmark">X</div></div></div>
+                    <div class="set"><i class="fas fa-question-circle" onclick="openModal(${wave[2+i]})"></i>${wave[2+i]} lbs x 1 reps <div class="xcheck_container"><div class="check">✓</div><div class="xmark" id="xmark">X</div></div></div>
                 </div>
                 `;  
             }
@@ -98,15 +98,77 @@ function waveTracker() {
                 `
                 <div> 
                     WAVE ${i+1}
-                    <div class="set">${wave[0+i]} lbs x 7 reps <div class="xcheck_container"><div class="check">✓</div><div class="xmark" id="xmark">X</div></div></div>
-                    <div class="set">${wave[1+i]} lbs x 5 reps <div class="xcheck_container"><div class="check">✓</div><div class="xmark" id="xmark">X</div></div></div>
-                    <div class="set">${wave[2+i]} lbs x 3 reps <div class="xcheck_container"><div class="check">✓</div><div class="xmark" id="xmark">X</div></div></div>
+                    <div class="set"><i class="fas fa-question-circle" onclick="openModal(${wave[0+i]})"></i>${wave[0+i]} lbs x 7 reps <div class="xcheck_container"><div class="check">✓</div><div class="xmark" id="xmark">X</div></div></div>
+                    <div class="set"><i class="fas fa-question-circle" onclick="openModal(${wave[1+i]})"></i>${wave[1+i]} lbs x 5 reps <div class="xcheck_container"><div class="check">✓</div><div class="xmark" id="xmark">X</div></div></div>
+                    <div class="set"><i class="fas fa-question-circle" onclick="openModal(${wave[2+i]})"></i>${wave[2+i]} lbs x 3 reps <div class="xcheck_container"><div class="check">✓</div><div class="xmark" id="xmark">X</div></div></div>
                 </div>
-                `;  
+                `;
             }
         }
         else {
             console.log("Not a valid input");
         }
     }
+}
+
+function closeModal() {
+    document.getElementById("modal").classList.add('hide');
+}
+function openModal(weight) {
+    a = 0; b = 0; c = 0; d = 0; e = 0; f = 0; g = 0; h = 0; i = 0;
+    plates=[a,b,c,d,e,f,g,h,i]
+    let oneside = (weight - 45)/2;
+    
+    // sets the 45 pound plate in the correct spot
+    plates[Math.floor(oneside/45) - 1 ] = 45; 
+
+    //set all plates before last 45 to 45
+    for (let z = 0; z < plates.length; z++) {
+        if (plates[plates.length - z] == 45) {
+            plates[plates.length - z - 1] = 45;
+        }
+    }
+
+    //set plates after 45
+    for (let z = 0 ; z < plates.length; z++) { //0,1,2,3,4,5,6,7,8
+        if (plates[z] != 45) {
+            if ( oneside%45 == 2.5) { plates[z] = 2;}
+            else if ( oneside%45 == 5) { plates[z] = 5;}
+            else if ( oneside%45 == 7.5) { plates[z] = 5; plates[z+1] = 2;}
+            else if ( oneside%45 == 10) { plates[z] = 10;}
+            else if ( oneside%45 == 12.5) { plates[z] = 10; plates[z+1] = 2;}
+            else if ( oneside%45 == 15) { plates[z] = 10; plates[z+1] = 5;}
+            else if ( oneside%45 == 17.5) { plates[z] = 10; plates[z+1] = 5; plates[z+2] = 2;}
+            else if ( oneside%45 == 20) { plates[z] = 10; plates[z+1] = 10;}
+            else if ( oneside%45 == 22.5) { plates[z] = 10; plates[z+1] = 10; plates[z+2] = 2;}
+            else if ( oneside%45 == 25) { plates[z] = 25;}
+            else if ( oneside%45 == 27.5) { plates[z] = 25; plates[z+1] = 2;}
+            else if ( oneside%45 == 30) { plates[z] = 25; plates[z+1] = 5;}
+            else if ( oneside%45 == 32.5) { plates[z] = 25; plates[z+1] = 5; plates[z+2] = 2;}
+            else if ( oneside%45 == 35) { plates[z] = 35;}
+            else if ( oneside%45 == 37.5) { plates[z] = 35; plates[z+1] = 2;}
+            else if ( oneside%45 == 40) { plates[z] = 35; plates[z+1] = 5;}
+            else if ( oneside%45 == 42.5) { plates[z] = 35; plates[z+1] = 5; plates[z+2] = 2;}
+            else { plates[z]=0; }
+            break;
+        }
+    }
+    
+
+    document.getElementById("modal").innerHTML = `
+        <div class="inner-modal">
+        <div class="close-modal" onclick="closeModal()"></div>
+        <div class="bar"></div>
+            <div class="plate plate-${plates[0]} spot-1"></div>
+            <div class="plate plate-${plates[1]} spot-2"></div>
+            <div class="plate plate-${plates[2]} spot-3"></div>
+            <div class="plate plate-${plates[3]} spot-4"></div>
+            <div class="plate plate-${plates[4]} spot-5"></div>
+            <div class="plate plate-${plates[5]} spot-6"></div>
+            <div class="plate plate-${plates[6]} spot-7"></div>
+            <div class="plate plate-${plates[7]} spot-8"></div>
+            <div class="plate plate-${plates[8]} spot-9"></div>
+        </div>
+    `
+    document.getElementById("modal").classList.remove('hide');
 }
