90f73f4293af5c7dc3d1491c612fe3396b5adb44
R370 <63675087+RafaelQuarles@users.noreply.github.com>
2022-03-16 13:20:51 -0400

    Added PWA functionality
    Turn the site into a progressive web app to be used offline as well

---
+++ sw.js
@@ -0,0 +1,15 @@
+self.addEventListener('install', function(event) {
+    event.waitUntil(
+        caches.open('sw-cache').then(function(cache) {
+            return cache.add('index.html', 'styles.css', 'dark.css', 'profiles/meetTheCrew.html', 'profiles/waveTraining.html', 'profiles/waveTraining.css');
+        })
+    );
+});
+
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+        caches.match(event.request).then(function(response) {
+            return response || fetch(event.request);
+        })
+    );
+});
