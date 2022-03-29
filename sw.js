f35eb8dcd471558b1731724f3ce12ed3606ea662
R370 <63675087+RafaelQuarles@users.noreply.github.com>
2022-03-29 10:15:27 -0400

    updates

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 self.addEventListener('install', function(event) {
     event.waitUntil(
         caches.open('sw-cache').then(function(cache) {
-            return cache.add('index.html', 'styles.css', 'profiles/meetTheCrew.html','profiles/meetTheCrew.css', 'profiles/waveTraining.html', 'profiles/waveTraining.css', 'profiles/waveTraining.js');
+            return cache.add('profiles/waveTraining.html', 'profiles/waveTraining.css', 'profiles/waveTraining.js');
         })
     );
 });
